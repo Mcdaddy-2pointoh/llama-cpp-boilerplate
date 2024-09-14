bd7ec34862db2952597bf949225d21ecb64a0d87
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-09-14 20:08:50 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llama_cpp_boilerplate',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://your-url.com',
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
